c88bafa9b277f202ffefadee51a05cb036b58941
Anibal Andrade <59257555+androso@users.noreply.github.com>
2022-03-03 22:53:09 +0000

    basic skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+import { useReducer } from 'react';
+import Chat from './Chat.jsx';
+import ContactList from './ContactList.jsx';
+import {
+  initialState,
+  messengerReducer
+} from './messengerReducer';
+
+export default function Messenger() {
+  const [state, dispatch] = useReducer(
+    messengerReducer,
+    initialState
+  );
+  const message = state.message;
+  const contact = contacts.find(c =>
+    c.id === state.selectedId
+  );
   return (
-    <main>
-      React⚛️ + Vite⚡ + Replit🌀
-    </main>
+    <div>
+      <ContactList
+        contacts={contacts}
+        selectedId={state.selectedId}
+        dispatch={dispatch}
+      />
+      <Chat
+        key={contact.id}
+        message={message}
+        contact={contact}
+        dispatch={dispatch}
+      />
+    </div>
   );
 }
 
-export default App;
+const contacts = [
+  { id: 0, name: 'Taylor', email: 'taylor@mail.com' },
+  { id: 1, name: 'Alice', email: 'alice@mail.com' },
+  { id: 2, name: 'Bob', email: 'bob@mail.com' }
+];
